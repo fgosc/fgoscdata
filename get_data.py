c23bee120adbb2d8b6eab6f1c2e070184e1c80b1
unknown <fgophi@gmail.com>
2020-11-22 13:49:51 +0900

    ガチャ産概念礼装の自動追加

--- get_data.py
+++ get_data.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import argparse
 import logging
-import requests
 import json
 from pathlib import Path
 import csv
+import unicodedata
+import re
+
+import requests
+from bs4 import BeautifulSoup
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
@@ -15,9 +19,55 @@ Item_blacklist_file = Path(__file__).resolve().parent / Path("item_bl.txt")
 shortname_file = Path(__file__).resolve().parent / Path("shortname.csv")
 CE_blacklist_file = Path(__file__).resolve().parent / Path("ce_bl.txt")
 CE_gacha_file = Path(__file__).resolve().parent / Path("ce_gacha.txt")
+ces = []
+
+def parse_page(load_url):
+    html = requests.get(load_url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    page_title = soup.find('title')
+    if "ピックアップ召喚" not in page_title.get_text():
+        return []
+    ces = []
+    items = soup.select(".gainen_ttl")
+    for item in items:
+        text = unicodedata.normalize('NFKC', item.get_text())
+        text =re.sub("\([^\(\)]*\)$", "", text.strip()).strip()
+        ces.append(text)
+    return ces
+
+
+def get_pages():
+    base_url = "https://news.fate-go.jp"
+    html = requests.get(base_url)
+    soup = BeautifulSoup(html.content, "html.parser")
+    tag_item = soup.select('ul.list_news li a')
+    ces = []
+
+    for tag in tag_item:
+        load_url = base_url + tag.get("href")
+        logger.debug(load_url)
+        try:
+            ce_list = parse_page(load_url)
+        except Exception as e:
+            logger.exception(e)
+            ce_list = None
+        if ce_list is not None:
+            ces += ce_list
+    return ces
+
+def is_gachaCe(ce):
+    """
+    ガチャ産CEか判別
+    """
+    if ce in ces:
+        return True
+    else:
+        return False
 
 
 def main(args):
+    global ces
+    ces = get_pages()
     # phash に存在しない(新)アイテム(id, name)の追記(shortname.csv, name_alias.csv)
     r_get = requests.get(item_url)
     item_list = r_get.json()
@@ -59,6 +109,10 @@ def main(args):
         # 除外礼装は読み込まない
         if name in bl_ces + gacha_ces:
             continue
+        # 公式ウェブのピックアップ召喚情報にある概念礼装を除外
+        if is_gachaCe(ce["name"]):
+            gacha_ces.append(ce["name"])
+            continue
         if ce["name"] not in name2shortname.keys():
             name2shortname[ce["name"]] = ""
 
@@ -67,6 +121,8 @@ def main(args):
         writer = csv.writer(f, lineterminator="\n")
         writer.writerow(["name", "shortname"])
         writer.writerows(rows)
+    with open(CE_gacha_file, "w", encoding='UTF-8') as f:
+        f.write('\n'.join(gacha_ces))
 
 
 def parse_args():
