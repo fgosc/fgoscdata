1253fd82e9a3027ee8ca59e5e47b4ebb917d13f8
unknown <fgophi@gmail.com>
2021-01-07 02:41:23 +0900

    sw2 return

--- extract_event.py
+++ extract_event.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 
 mstQuest_url = "https://raw.githubusercontent.com/FZFalzar/FGOData/master/JP_tables/quest/mstQuest.json"
 mstQuestInfo_url = "https://raw.githubusercontent.com/FZFalzar/FGOData/master/JP_tables/quest/viewQuestInfo.json"
+#mstQuestInfo_url = "https://raw.githubusercontent.com/FZFalzar/FGOData/81542cc7fa715efbc55fca95b4fffea5fcf1450a/JP_tables/quest/viewQuestInfo.json"
 mstQuestPhase_url = "https://raw.githubusercontent.com/FZFalzar/FGOData/master/JP_tables/quest/mstQuestPhase.json"
 
 
@@ -26,10 +27,12 @@ def list2class(enemy):
     return out
 
 def main(args):
-    opened = int(datetime.datetime.strptime(args.openedat, "%Y/%m/%d").timestamp())
-    closed = int(datetime.datetime.strptime(args.closedat + ' 23:59:59', "%Y/%m/%d %H:%M:%S").timestamp())
-    logger.debug("opendat: %s", opened)
-    logger.debug("closedat: %s", closed)
+    if args.openedat:
+        opened = int(datetime.datetime.strptime(args.openedat, "%Y/%m/%d").timestamp())
+        logger.debug("opendat: %s", opened)
+    if args.closedat:
+        closed = int(datetime.datetime.strptime(args.closedat + ' 23:59:59', "%Y/%m/%d %H:%M:%S").timestamp())
+        logger.debug("closedat: %s", closed)
 
     r_get = requests.get(mstQuest_url)
     mstQuest_list = r_get.json()
@@ -41,12 +44,19 @@ def main(args):
 
     q_list = []
     for quest in mstQuest_list:
-        if opened <= quest["openedAt"] and quest["closedAt"] <= closed:
-            for q in mstQuestInfo_list:
-                if q["questId"] == quest["id"] and "高難易度" not in quest["name"]:
-                    enemy = questId2classIds[quest["id"]]
-                    q_list.append([quest["id"], quest["name"], quest["recommendLv"], list2class(enemy)])
-                    break
+        # if opened <= quest["openedAt"] and quest["closedAt"] <= closed:
+        if not (94000000 < quest["id"] < 100000000):
+            continue
+        if "種火集め" in quest["name"] or "宝物庫" in quest["name"] or  "修練場" in quest["name"]:
+            continue
+        if quest["closedAt"] == 1901199599:
+            continue
+        logger.info(quest["id"])
+        for q in mstQuestInfo_list:
+            if q["questId"] == quest["id"] and "高難易度" not in quest["name"]:
+                enemy = questId2classIds[quest["id"]]
+                q_list.append([quest["id"], quest["name"], quest["recommendLv"], list2class(enemy)])
+                break
 
     with open("event_list.csv", "w", encoding='UTF-8') as f:
         writer = csv.writer(f, lineterminator="\n")
@@ -56,10 +66,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='FGOクエストをCSV出力')
     parser.add_argument('-o', '--openedat',
-                        required=True,
                         help='quest open date: e.g. 2015/7/30')
     parser.add_argument('-c', '--closedat',
-                        required=True,
                         help='quest close date: e.g.  2020/11/03')
     parser.add_argument('-l', '--loglevel',
                         choices=('debug', 'info'), default='info')
